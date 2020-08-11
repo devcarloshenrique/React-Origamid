b8ea47ab08dc1c6469b8ce76a910e950d3dadc43
devcarloshenrique <devcarloshenrique@gmail.com>
2020-08-11 15:14:49 -0300

    refactor

---
+++ revisaoJs/expressao.js
@@ -0,0 +1,14 @@
+const grupoA = 100;
+const grupoB = 200;
+
+if (grupoA > grupoB) {
+  console.log('Grupo A, ganhou');
+} else {
+  console.log('Grupo B, ganhou');
+}
+
+//Operador tercenario, é uma expressão
+
+const vencedor = grupoA > grupoB ? 'Grupo A Venceu' : 'Grupo B Venceu';
+
+console.log(vencedor);

bf067eb287a7ab224a9087a80d8288f3999291bf
devcarloshenrique <devcarloshenrique@gmail.com>
2020-11-17 14:13:34 -0300

    feat: css import

---
+++ Modulo 5 - Css/appreact/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Title from './Title';
+
+function App() {
+  return (
+    <div className="container">
+      <Title />
+    </div>
+  );
+}
+
+export default App;
